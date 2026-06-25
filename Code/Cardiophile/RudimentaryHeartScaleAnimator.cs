b69bff84cc9390c7bb5b555ec020e51e3b550e02
JOELwindows7 <JOELwindows7@users.noreply.github.com>
2026-06-25 20:36:02 +0700

    aaa

---
+++ Code/Cardiophile/RudimentaryHeartScaleAnimator.cs
@@ -0,0 +1,27 @@
+namespace Sandbox;
+
+public sealed class RudimentaryHeartScaleAnimator : Component
+{
+	[Property, Feature( "Properties" )] public float enlargeTo = 1.2f;
+	[Property, Feature( "Properties" )] public float relaxTo = 1f;
+
+	[Property, RequireComponent] HeartOrgan Itself { get; set; }
+	[Property] GameObject ModelObject { get; set; }
+
+	protected override void OnUpdate()
+	{
+		if(Itself.IsValid())
+		{
+			if(ModelObject.IsValid())
+			{
+				// if ( Itself.Lub ) ModelObject.WorldScale = enlargeTo;
+				if ( Itself.Lub ) ModelObject.WorldScale = Vector3.Lerp(ModelObject.WorldScale,enlargeTo,.02f);
+				// else ModelObject.WorldScale = relaxTo;
+				else ModelObject.WorldScale =  Vector3.Lerp(ModelObject.WorldScale,relaxTo,.02f); ;
+
+				//DONE: Lerp!!!
+				// TODO: Woman 3d model, prioritize furry ones, such as horse thyren or subspecies like pony hasbro
+			}
+		}
+	}
+}
