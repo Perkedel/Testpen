cf7d9fc9937c0511034bd66d102f67f8f811df5b
JOELwindows7 <JOELwindows7@users.noreply.github.com>
2026-08-03 12:10:07 +0700

    .

--- Code/Cardiophile/HeartOrgan.cs
+++ Code/Cardiophile/HeartOrgan.cs
@@ -21,6 +21,24 @@ public sealed class HeartOrgan : Component
 	[Property, Feature("Properties")] public float HeartRate { get; set; } = 70f; // Star of the show!
 	[Property, Feature("Properties")] public float IdealRate { get; set; } = 70f; // Homo sapiens heart rate ideally is
 	[Property, Feature( "Properties" )] public float MaximumRate { get; set; } = 500f; // Maximum BPM cap from normal doing
+	[Header( "Link another heart organs wirelessly!" )]
+	[Property, Feature( "Properties" ), Group( "Linking" )] public HeartOrgan[] OtherHearts { get; set; } // Useful for Linked List MultiHearts, or Quantumly Entangled Twins.
+	[Header( "Adjust Funny effect chances here" )]
+	[Property, Feature( "Properties" ), Group("Variance"),Advanced] public bool DiastoleReturnDeviates { get; set; } = false; // Is Diastole time deviates?
+	[Property, Feature( "Properties" ), Group("Variance"),Advanced] public float DiastoleReturnDeviation { get; set; } = .3f; // How much Return Time deviates Plus minus from Decide Return?
+	[Property, Feature( "Properties" ), Group("Hiccup"),Advanced] public float SkipChancePercent { get; set; } = 0f; // Chance of unexpected extended silence Period Time
+	[Property, Feature( "Properties" ), Group("Hiccup"),Advanced] public float SkipSetTime { get; set; } = 2f; // and for how long it set instead
+	[Property, Feature( "Properties" ), Group("Hiccup"),Advanced] public float SkipSetTimeMin { get; set; } = 2f; // Minimum for random decide skip set time
+	[Property, Feature( "Properties" ), Group( "Hiccup" ),Advanced] public float SkipSetTimeMax { get; set; } = 3f; // & max
+	[Property, Feature( "Properties" ), Group("Hiccup"),Advanced] public float PVCSetTime { get; set; } = .1f; // and for how long it beats too early per previous PVC session
+	[Property, Feature( "Properties" ), Group("Hiccup"),Advanced] public float PVCSetTimeMin { get; set; } = .1f; // Minimum for random decide to beats too early
+	[Property, Feature( "Properties" ), Group("Hiccup"),Advanced] public float PVCSetTimeMax { get; set; } = .3f; // & max
+	[Property, Feature( "Properties" ), Group("Hiccup"),Advanced] public float PostPVCSetTime { get; set; } = .1f; // and for how long it diastole too early after that skip
+	[Property, Feature( "Properties" ), Group("Hiccup"),Advanced] public float PostPVCSetTimeMin { get; set; } = .1f; // Minimum for random decide to diastole too early time
+	[Property, Feature( "Properties" ), Group("Hiccup"),Advanced] public float PostPVCSetTimeMax { get; set; } = .3f; // & max
+	[Property, Feature( "Properties" ), Group("Hiccup"),Advanced] public float PVCChancePercent { get; set; } = 0f; // Chance of PVC
+	[Property, Feature( "Properties" ), Group("Hiccup"),Advanced] public int PVCCountPerSessionMin { get; set; } = 2; // and how many PVC typically
+	[Property, Feature( "Properties" ), Group("Hiccup"),Advanced] public int PVCCountPerSessionMax { get; set; } = 2; // and max
 
 	//[Property, Feature("Properties")] public Ecghud ECGMonitor { get; set; } // Insert ECG here!
 
@@ -39,15 +57,47 @@ public sealed class HeartOrgan : Component
 	[Property, Feature("Extra Debugs"), Group("Mathematics"), ReadOnly] public float ReturnTime { get; set; } = .25f; // When heart will diastole back?
 	[Property, Feature("Extra Debugs"), Group("Running"), ReadOnly] public float StartReturnTime { get; set; } = .25f; // catch ReturnTime now & reset based ReturnTime
 	[Property, Feature( "Extra Debugs" ), Group( "Running Conversion" ), ReadOnly] float StartReturnTimeMillisec { get; set; } = (.25f) * 1000f; // StartReturnTime * 1000
+	[Property, Feature("Extra Debugs"), Group("Mathematics"), ReadOnly] public float PVCPeriodT { get; set; } = 5f; // for PVC event
 
 	[Header("Here's the Core of it!")]
 	[Property, Feature("Extra Debugs"), Group("Core")] public bool Lub { get; set; } = false; // Heart Systole
-	[Property, Feature("Extra Debugs"), Group("Core"), ReadOnly] int StateIndex { get; set; } = 0; // Heart organ state
+	[Property, Feature( "Extra Debugs" ), Group( "Core" ), ReadOnly] int StateIndex { get; set; } = 0; // Heart organ state
+	/*
+	- 0
+	- 1
+	- 2
+	- 3
+	- 10 PVC Lub
+	- 11 PVC Dub
+	- 12 Awkward stun / Skip; hmmm use glitched out unexpected increased period time
+	*/
 	[Property, Feature("Extra Debugs"), Group("Core")] bool isBeating { get; set; } = true; // Heart is alive
 	[Property, Feature( "Extra Debugs" ), Group( "Info" ), ReadOnly] string ToggleSay { get; set; } = ""; // Heart text info
 
+	[Header( "Also Funny effect queue lines" )]
+	// [Property, Feature( "Extra Debugs" ), Group( "Hiccups" ), ReadOnly] public uint PrematureVentricularContractions { get; set; } = 0; // Heart had to PVC, pop number
+	[Property, Feature( "Extra Debugs" ), Group( "Hiccups" ), ReadOnly] public uint SkipQueue { get; set; } = 0; // Heart had to skip, pop number
+	// var DoesPVC ? -> the heart indeed is dadug dadug, not just a pause
+	[Property, Feature( "Extra Debugs" ), Group( "Hiccups" )] public bool SkipInduced { get; set; } = false; // Manually induce skip
+	[Property, Feature( "Extra Debugs" ), Group( "Hiccups" )] public bool WithPVC { get; set; } = false; // (argument for Skip) that is dadug-dadug
+	[Property, Feature( "Extra Debugs" ), Group( "Hiccups" ),ReadOnly] public bool GoingToSkip { get; set; } = false; // buffer for next beat will skip
+	[Property, Feature( "Extra Debugs" ), Group( "Hiccups" ), ReadOnly] public bool SkipActive { get; internal set; } = false; // Heart is skipping, which is a
+	[Property, Feature( "Extra Debugs" ), Group( "Hiccups" ), ReadOnly] public bool SkipDone { get; internal set; } = false; // Heart is skipping, which is a
+	[Property, Feature( "Extra Debugs" ), Group( "Hiccups" ), ReadOnly] public bool IsPVC { get; internal set; } = false; // PVC?, which is Dadug-dadug? false = just a pause
+	// [Property, Feature( "Extra Debugs" ), Group( "Hiccups" ), ReadOnly] public bool PVCActive { get; internal set; } = false; // in PVC mode
+	// [Property, Feature( "Extra Debugs" ), Group( "Hiccups" ), ReadOnly] public bool PVCDone { get; internal set; } = false; // is it done yet?
+	[Property, Feature( "Extra Debugs" ), Group( "Hiccups" ), ReadOnly] public int PVCNumbersInThisSession { get; set; } = 2; // How many PVC left now
+	[Property, Feature( "Extra Debugs" ), Group( "Hiccups" ), ReadOnly] public float DiceRollSkip { get; set; } = 0f; // Roll the dice
+
 	[Property, Feature("Scripting"), Group("Cookie Clicker")] public Doo OnLub{ get; set; }
 	[Property, Feature( "Scripting" ), Group( "Rudimentary" )] public Ecghud Ecg { get; set; }
+
+	public float NextSingle(float min = 0f, float max = 0f)
+	{
+		// https://gist.github.com/MachineCharmer/941949
+		// http://stackoverflow.com/questions/1064901/random-number-between-2-double-numbers/1064907#1064907
+		return Game.Random.NextSingle() * (max-min) + min;
+	}
 
 	public void HandoverEcg( Ecghud itThis )
 	{
@@ -56,10 +106,22 @@ public sealed class HeartOrgan : Component
 
 	protected void OnLubFunc()
 	{
-		if(Ecg.IsValid())
+		if ( Ecg.IsValid() )
 		{
 			Ecg.CookieClick();
 		}
+	}
+
+	// protected void DecideLubTime()
+	// {
+
+	// }
+
+	public void InduceSkip(bool withPVC = false)
+	{
+		WithPVC = withPVC;
+		GoingToSkip = true;
+		// SkipInduced = true;
 	}
 
 	protected void DecideReturnTime( float forWhathowMuch = 0 )
@@ -68,17 +130,17 @@ public sealed class HeartOrgan : Component
 		{
 			// you are dead, not a big surprise.
 			ToggleSay = "X_X Eik Serkat!";
-			ReturnTime = 0f;
+			ReturnTime = 0f ;
 		}
 		else if ( forWhathowMuch >= 1 && forWhathowMuch < 20 )
 		{
 			ToggleSay = "...";
-			ReturnTime = .75f;
+			ReturnTime = .75f + (DiastoleReturnDeviates? NextSingle(-DiastoleReturnDeviation,DiastoleReturnDeviation) : 0);
 		}
 		else if ( forWhathowMuch >= 20 && forWhathowMuch < 50 )
 		{
 			ToggleSay = "Looooooooww... heeeaarrt raaaaaate...";
-			ReturnTime = .5f;
+			ReturnTime = .5f + (DiastoleReturnDeviates? NextSingle(-DiastoleReturnDeviation,DiastoleReturnDeviation) : 0);
 		}
 		else if ( forWhathowMuch >= 50 && forWhathowMuch < 70 )
 		{
@@ -86,42 +148,42 @@ public sealed class HeartOrgan : Component
 			if ( forWhathowMuch == 69 ) ToggleSay = "nice";
 			else if ( forWhathowMuch == 67 ) ToggleSay = "bruh";
 			else ToggleSay = "Sleepie";
-			ReturnTime = .3f;
+			ReturnTime = .3f + (DiastoleReturnDeviates? NextSingle(-DiastoleReturnDeviation,DiastoleReturnDeviation) : 0);
 		}
 		else if ( forWhathowMuch >= 70 && forWhathowMuch < 90 )
 		{
 			ToggleSay = "Heartbeat";
-			ReturnTime = .25f;
+			ReturnTime = .25f + (DiastoleReturnDeviates? NextSingle(-DiastoleReturnDeviation,DiastoleReturnDeviation) : 0);
 		}
 		else if ( forWhathowMuch >= 90 && forWhathowMuch < 100 )
 		{
 			ToggleSay = "Accelerated";
-			ReturnTime = .20f;
+			ReturnTime = .20f + (DiastoleReturnDeviates? NextSingle(-DiastoleReturnDeviation,DiastoleReturnDeviation) : 0);
 		}
 		else if ( forWhathowMuch >= 100 && forWhathowMuch < 150 )
 		{
 			ToggleSay = "FASS";
-			ReturnTime = .15f;
+			ReturnTime = .15f + (DiastoleReturnDeviates? NextSingle(-DiastoleReturnDeviation,DiastoleReturnDeviation) : 0);
 		}
 		else if ( forWhathowMuch >= 150 && forWhathowMuch < 200 )
 		{
 			ToggleSay = "VERY FASS";
-			ReturnTime = .1f;
+			ReturnTime = .1f + (DiastoleReturnDeviates? NextSingle(-DiastoleReturnDeviation,DiastoleReturnDeviation) : 0);
 		}
 		else if ( forWhathowMuch >= 200 && forWhathowMuch < 300 )
 		{
 			ToggleSay = "TOO FASS";
-			ReturnTime = .05f;
+			ReturnTime = .05f + (DiastoleReturnDeviates? NextSingle(-DiastoleReturnDeviation,DiastoleReturnDeviation) : 0) * .01f;
 		}
 		else if ( forWhathowMuch >= 300 && forWhathowMuch < 400 )
 		{
 			ToggleSay = "EXTREMELY FASS";
-			ReturnTime = .025f;
+			ReturnTime = .025f + (DiastoleReturnDeviates? NextSingle(-DiastoleReturnDeviation,DiastoleReturnDeviation) : 0) * .01f;
 		}
 		else if ( forWhathowMuch >= 400 )
 		{
 			ToggleSay = "OH PECK!!! FIBRILATION GOING ON!!! OH NO!!!";
-			ReturnTime = .001f;
+			ReturnTime = .001f + (DiastoleReturnDeviates? NextSingle(-DiastoleReturnDeviation,DiastoleReturnDeviation) : 0) * .001f;
 		}
 		else
 		{
@@ -174,19 +236,101 @@ public sealed class HeartOrgan : Component
 			HeartRate = 0;
 			_Hr = 0;
 		}
-		if ( _Hr >= CriticalMaxRate)
+		if ( _Hr >= CriticalMaxRate )
 		{
 			HeartRate = CriticalMaxRate;
 			_Hr = CriticalMaxRate;
 		}
-		DecideReturnTime( _Hr );
-		Hertz = _Hr > 0 ? _Hr / 60 : 1;
-		PeriodT = Hertz > 0 ? 1 / Hertz : 1;
+		if(!SkipActive || SkipDone)
+		{
+			DecideReturnTime( _Hr );
+			Hertz = _Hr > 0 ? _Hr / 60 : 1;
+			PeriodT = Hertz > 0 ? 1 / Hertz : 1;
+		} else
+		{
+			Hertz = _Hr > 0 ? _Hr / 60 : 1;
+			// if(SkipActive && SkipDone)
+			// {
+			// 	SkipActive = false;
+			// 	SkipDone = false;
+			// }
+		}
+	}
+
+	protected void AsyncHeartInteruptUpdate( float Delta )
+	{
+		// Now if there is pending hiccups used, here procedure!
+
+
+		// if ( Lub )
+		// {
+
+		// }
+
+		// if(PVCDone)
+		// {
+		// 	// Pop the queue!
+		// 	if ( PrematureVentricularContractions > 0 ) PrematureVentricularContractions--; // only pop when is more than 0
+		// 	// otherwise you'd roll over to 2^32 whatever this is.
+		// 	if ( PrematureVentricularContractions < 0 ) PrematureVentricularContractions = 0; // no I don't think that's how uint work..
+		// }
+	}
+
+	protected void DazedHeartbeatSubsystem( float Delta)
+	{
+		if ( ((SkipChancePercent > 0f && DiceRollSkip < SkipChancePercent) && !SkipActive) || GoingToSkip )
+		{
+			// Brainstormed by Joel. that's it. Only a human.
+			SkipActive = true;
+			SkipInduced = false;
+			GoingToSkip = false;
+			IsPVC = WithPVC;
+
+			// decide skip time
+			// SkipSetTime = Game.Random.Next(SkipSetTimeMin,SkipSetTimeMax);
+			// SkipSetTime = Game.Random.NextSingle();
+			// bro
+			// ~~https://medium.com/analytics-vidhya/random-floats-in-any-range-9b40d30b637b~~
+			// https://gist.github.com/MachineCharmer/941949
+			// http://stackoverflow.com/questions/1064901/random-number-between-2-double-numbers/1064907#1064907
+			// why the fuck no range?!?!?
+			SkipSetTime = Game.Random.NextSingle() * (SkipSetTimeMax - SkipSetTimeMin) + SkipSetTimeMin;
+			Log.Info( $"Skip Set time is {SkipSetTime} " );
+			PVCSetTime = Game.Random.NextSingle() * (PVCSetTimeMax - PVCSetTimeMin) + PVCSetTimeMin;
+			PostPVCSetTime = Game.Random.NextSingle() * (PostPVCSetTimeMax - PostPVCSetTimeMin) + PostPVCSetTimeMin;
+
+			// _capT += 12f;
+			// _capT = PVCSetTime;
+			PeriodT = IsPVC? PVCSetTime : SkipSetTime;
+			RemainPeriodT = IsPVC? PVCSetTime : SkipSetTime;
+			ReturnTime = IsPVC? PostPVCSetTime : ReturnTime;
+			// StartReturnTime = PostPVCSetTime;
+
+			// if ( SkipQueue > 0 ) SkipQueue--;
+			// if ( SkipQueue < 0 ) SkipQueue = 0;
+			PVCNumbersInThisSession = Game.Random.Next( PVCCountPerSessionMin, PVCCountPerSessionMax );
+		}
+		else
+		{
+			if ( SkipActive )
+			{
+
+			}
+		}
+		// return void;
 	}
 
 	protected void AsyncHeartbeatUpdate( float Delta )
 	{
 		float _capT = Math.Min( RemainPeriodT, 1 / (HeartRate / 60) ); // cap the perioding!
+
+		// but if heart had to skip a beat
+		DazedHeartbeatSubsystem( Delta );
+		if(SkipActive)
+		{
+			_capT = RemainPeriodT;
+		}
+
 		if ( RemainPeriodT > _capT )
 		{
 			RemainPeriodT = _capT;
@@ -196,6 +340,13 @@ public sealed class HeartOrgan : Component
 		RemainPeriodTMillisec = RemainPeriodT * 1000f;
 		if ( RemainPeriodTMillisec <= 0 )
 		{
+			// But before systole, check if there is Hiccup on queue
+			// if(PrematureVentricularContractions > 0)
+			// {
+			// 	// interupt & go to this special procedure instead!
+			// 	// return
+			// }
+
 			// Heart Systole
 			StateIndex = 1;
 			RemainPeriodT = PeriodT;
@@ -208,6 +359,44 @@ public sealed class HeartOrgan : Component
 			}
 			RunDoo( OnLub );
 			OnLubFunc();
+			if(SkipActive)
+			{
+				if ( IsPVC )
+					// check count phase
+					if(PVCNumbersInThisSession > 0)
+					{
+						// ongoing count
+						PVCSetTime = Game.Random.NextSingle() * (PVCSetTimeMax - PVCSetTimeMin) + PVCSetTimeMin;
+						PostPVCSetTime = Game.Random.NextSingle() * (PostPVCSetTimeMax - PostPVCSetTimeMin) + PostPVCSetTimeMin;
+
+						// _capT = PVCSetTime;
+						PeriodT = PVCSetTime;
+						// RemainPeriodT = PVCSetTime;
+						ReturnTime = PostPVCSetTime;
+
+						if ( PVCNumbersInThisSession > 0 ) PVCNumbersInThisSession--;
+						if ( PVCNumbersInThisSession < 0 ) PVCNumbersInThisSession = 0;
+					} else
+					{
+						// this is the last one!
+						// SkipSetTime = Game.Random.NextSingle() * (SkipSetTimeMax - SkipSetTimeMin) + SkipSetTimeMin;
+						// _capT = SkipSetTime;
+						PeriodT = SkipSetTime;
+						// RemainPeriodT = SkipSetTime;
+						// so it pauses really
+						// SkipActive = false;
+						SkipDone = true;
+					}
+
+				else
+				{
+					// Just a pause then? okay let's call it immediately.
+					// _capT = SkipSetTime;
+					PeriodT = SkipSetTime;
+					// RemainPeriodT = SkipSetTime;
+					SkipDone = true;
+				}
+			}
 		}
 		else
 		{
@@ -229,6 +418,32 @@ public sealed class HeartOrgan : Component
 				if ( EnableSound )
 				{
 					if ( DiastoleSound.IsValid() ) Sound.Play( DiastoleSound, WorldPosition );
+				}
+				if(SkipActive)
+				{
+
+
+					if(SkipActive && SkipDone)
+					{
+						PeriodT = SkipSetTime;
+						RemainPeriodT = PeriodT;
+						SkipActive = false;
+						SkipDone = false;
+						// heart got it together again
+						// ResyncHeartUpdate( Delta );
+					}
+				} else
+				{
+					// check if heart will skip
+					// if ( (( SkipChancePercent > 0f && DiceRollSkip < SkipChancePercent ) && !SkipActive) || SkipInduced )
+					// {
+					// 	DazedHeartbeatSubsystem( Delta );
+					// }
+					if ( GoingToSkip ) SkipActive = true;
+					// Refresh Remain Period T, partial resync
+					// RemainPeriodT = PeriodT;
+					// dice roll for next beat
+					DiceRollSkip = Game.Random.NextSingle() * 100f;
 				}
 			}
 			else
